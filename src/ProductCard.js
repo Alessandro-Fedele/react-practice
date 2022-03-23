e97ed3b474a8d40ea71f2eeda9fb810c3a55a22e
Alessandro-Fedele <al.fedele@outlook.it>
2022-03-23 13:02:54 +0100

    event

--- src/ProductCard.js
+++ src/ProductCard.js
@@ -6,6 +6,17 @@ const ProductCard = (props) =>
     // Con lo spread operator è ottimo usare la decostruzione dell'oggetto per prendere le props
     const { nome, img, alt, price, colore } = props;
 
+    // funzione per onclick
+    const logPrezzo = (price) =>
+    {
+        console.log(price);
+    };
+
+    const eventHandler = (ev) =>
+    {
+        console.log(ev.target.innerText);
+    };
+
     return (
         // Adesso invece di usare props. posso passare direttamente le chiavi che mi occorrono che ho messo a riga 7
 
@@ -13,7 +24,12 @@ const ProductCard = (props) =>
             <h3>{nome}</h3>
             <img src={img} alt={alt} />
             <h4>Price: {price}</h4>
-            <button>Acquista</button>
+            <button onClick={() => console.log({ price })}>Acquista</button>
+
+            <button onClick={() => logPrezzo(price)}>Acquista ora</button>
+
+            <button onClick={eventHandler}>Event Handler</button>
+
             <p>Extra: {props.children}</p>
         </div>
 
